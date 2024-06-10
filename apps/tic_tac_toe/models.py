bdf07648a01a9f1f294796be500dd9aa042f4960
Doppelgangeres <artyom.kovalenko003@gmail.com>
2024-06-10 20:31:40 +0300

    add database

--- apps/tic_tac_toe/models.py
+++ apps/tic_tac_toe/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_open = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Комната TicTacToe'
+        verbose_name_plural = 'Комнаты TicTacToe'
